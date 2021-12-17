945e0002585e96a0feeb51faa155eaed7cadebd5
ElinkaD <93198265+ElinkaD@users.noreply.github.com>
2021-12-17 17:27:07 +0300

    Delete Story directory

--- Story/src/main/Message.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public interface Message {
-    void speak();
-}
--- Story/src/main/StateWeatherException.java
+++
@@ -1,7 +0,0 @@
-package main;
-
-public class StateWeatherException extends RuntimeException{
-    public StateWeatherException(String message){
-        super(message);
-    }
-}
